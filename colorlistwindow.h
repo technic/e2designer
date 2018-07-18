5a37ec5d240d6e2cddde0e76ad252f0508f54fa7
Alex Maystrenko <alexeytech@gmail.com>
2018-07-18 23:40:14 +0200

    fix QtColorWidgets include path

--- colorlistwindow.h
+++ colorlistwindow.h
@@ -6,7 +6,7 @@
 #include <QDataWidgetMapper>
 #include <QAbstractItemModel>
 
-#include "Qt-Color-Widgets/include/ColorWheel"
+#include "Qt-Color-Widgets/include/QtColorWidgets/ColorWheel"
 
 namespace Ui {
 class ColorListWindow;
