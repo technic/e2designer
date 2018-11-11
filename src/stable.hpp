a04445d906117737235eb57eb06aa945bb433f9d
Alex Maystrenko <alexeytech@gmail.com>
2018-11-11 22:15:40 +0100

    enable precompiled header

---
+++ src/stable.hpp
@@ -0,0 +1,15 @@
+// Precompiled header
+#include <QObject>
+#include <QVector>
+#include <QHash>
+#include <QList>
+#include <QColor>
+#include <QFont>
+#include <QVariant>
+#include <QDir>
+#include <QFile>
+#include <QAbstractItemModel>
+#include <QAbstractTableModel>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
